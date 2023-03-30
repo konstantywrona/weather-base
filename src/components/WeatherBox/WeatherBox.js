bf7bfaae527f3c12380b7d740a45178ce4b3126a
Konstanty Wrona <koswro@gmail.com>
2023-03-30 18:07:20 +0200

    Add weather state 17.1 etap2

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,33 +1,35 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
 const WeatherBox = (props) => {
+  const [weather, setWeather] = useState('');
+
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const handleCityChange = useCallback((city) => {
     console.log(city);
 
     fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4df6cf57259d3a79ac5e0fead44f9210&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3ddedd4ebc6f1c8eaa18b8a00a62d417&units=metric`
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        const weatherData = {
+          city: data.name,
+          temp: data.main.temp,
+          icon: data.weather[0].icon,
+          description: data.weather[0].main,
+        };
+        setWeather(weatherData);
+        console.log(weatherData);
       });
   });
-
-  const weatherData = {
-    city: data.name,
-    temp: data.main.temp,
-    icon: data.weather[0].icon,
-    description: data.weather[0].main,
-  };
 
   return (
     <section>
       <PickCity action={handleCityChange} />
-      <WeatherSummary />
+      {<WeatherSummary {...weather} />}
       <Loader />
     </section>
   );
