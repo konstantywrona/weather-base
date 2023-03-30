19305573098e443c192399d761a8c704efc8a289
Konstanty Wrona <koswro@gmail.com>
2023-03-30 18:33:03 +0200

    Add error handeling 17.1 etap4

--- src/components/ErrorBox/ErrorBox.js
+++ src/components/ErrorBox/ErrorBox.js
@@ -1,22 +1,20 @@
 import PropTypes from 'prop-types';
 import styles from './ErrorBox.module.scss';
 
-const ErrorBox = ({ children }) => {
+const ErrorBox = () => {
   return (
     <div className={styles.errorBox}>
       <h1>
         <span className="fa fa-exclamation-triangle" />
         Error
       </h1>
-      <p>
-        {children}
-      </p>
+      <p>There is no such city</p>
     </div>
   );
 };
 
-ErrorBox.propTypes = { 
-  children: PropTypes.string
+ErrorBox.propTypes = {
+  children: PropTypes.string,
 };
 
 export default ErrorBox;
--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -2,38 +2,47 @@ import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
 import { useCallback, useState } from 'react';
+import ErrorBox from '../ErrorBox/ErrorBox';
 
 const WeatherBox = (props) => {
   const [weather, setWeather] = useState('');
   const [pending, setPending] = useState(false);
+  const [error, setError] = useState(false);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const handleCityChange = useCallback((city) => {
     setPending(true);
+    setError(false);
     console.log(city);
 
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3ddedd4ebc6f1c8eaa18b8a00a62d417&units=metric`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        const weatherData = {
-          city: data.name,
-          temp: data.main.temp,
-          icon: data.weather[0].icon,
-          description: data.weather[0].main,
-        };
-        setPending(false);
-        setWeather(weatherData);
-        console.log(weatherData);
-      });
+    ).then((res) => {
+      if (res.status === 200) {
+        return res.json().then((data) => {
+          const weatherData = {
+            city: data.name,
+            temp: data.main.temp,
+            icon: data.weather[0].icon,
+            description: data.weather[0].main,
+          };
+
+          setPending(false);
+          setWeather(weatherData);
+          console.log(weatherData);
+        });
+      } else {
+        setError(true);
+      }
+    });
   });
 
   return (
     <section>
       <PickCity action={handleCityChange} />
       {weather && !pending && <WeatherSummary {...weather} />}
-      {pending && <Loader />}
+      {pending && !error && <Loader />}
+      {error && <ErrorBox />}
     </section>
   );
 };
