b84aced6f1c475f8688a794d798375a4b7d22cb0
Konstanty Wrona <koswro@gmail.com>
2023-03-30 17:44:27 +0200

    Add handleSubmit handleCityChange fetch API 17.1 zadanie  etap1

--- src/components/PickCity/PickCity.js
+++ src/components/PickCity/PickCity.js
@@ -4,13 +4,23 @@ import styles from './PickCity.module.scss';
 
 import { useState } from 'react';
 
-const PickCity = () => {
+const PickCity = ({ action }) => {
   const [city, setCity] = useState('');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    action(city);
+    setCity('');
+  };
+
   return (
-    <form className={styles.pickCityForm}> 
+    <form className={styles.pickCityForm} onSubmit={handleSubmit}>
       <label>
-        <TextInput placeholder="Enter city name...." value={city} onChange={e => setCity(e.target.value)} />
+        <TextInput
+          placeholder="Enter city name...."
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
       </label>
       <Button>Search</Button>
     </form>
--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,15 +1,36 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
+import { useCallback } from 'react';
 
-const WeatherBox = props => {
+const WeatherBox = (props) => {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const handleCityChange = useCallback((city) => {
+    console.log(city);
+
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4df6cf57259d3a79ac5e0fead44f9210&units=metric`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  });
+
+  const weatherData = {
+    city: data.name,
+    temp: data.main.temp,
+    icon: data.weather[0].icon,
+    description: data.weather[0].main,
+  };
+
   return (
     <section>
-      <PickCity />
+      <PickCity action={handleCityChange} />
       <WeatherSummary />
       <Loader />
     </section>
-  )
+  );
 };
 
 export default WeatherBox;
